3691a33b3f75e406be65291f877fe1015bf07906
NathiMajaja <nkosinathi.majaja24@gmail.com>
2022-10-21 02:21:05 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,5 +12,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int _putchar(char c);
+
 
 #endif
