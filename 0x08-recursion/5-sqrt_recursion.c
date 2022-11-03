a8207887be9a8e3cc1103e981436c1a8bbb9a91c
NathiMajaja <nkosinathi.majaja24@gmail.com>
2022-11-03 11:32:08 -0700

    Inception. Is it possible?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,13 +4,15 @@ int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
 
 /*
- * _sqrt_recursion - returns the natural square root of a number
+ * find_sqrt - finds the natural square root of an inputted number
  * @num: the number to find the square root of
  * @root: the root to be tested
  * Return: If the number has a natural square root - the square root
  *	If the number does not have a natural square root - -1.
  */
+
 int find_sqrt(int num, int root)
+
 {
 	if ((root * root) == num)
 		return (root);
