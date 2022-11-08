d0107b46216c31c237e6f4dcab3cf0ffd1f7e2e6
NathiMajaja <nkosinathi.majaja24@gmail.com>
2022-11-08 08:08:03 -0800

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,7 +22,7 @@ char *create_array(unsigned int size, char c)
 		position = 0;
 		while (position < size)
 		{
-			*(buffer) = c;
+			*(buffer + position) = c;
 			position++;
 		}
 		return (buffer);
